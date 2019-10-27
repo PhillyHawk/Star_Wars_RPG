e079668061f827289e65abbe050c15848198ee9b
Hawk Shannon <hawk.dillinger@gmail.com>
2019-10-27 13:04:29 -0400

    added characters

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+
+
+var characters = {
+  "Obi-Wan Kenobi": {
+    name: "Obi-Wan Kenobi",
+    health : 120,
+    attack: 8,
+    inageUrl: "assets/images/obi-wan.jpg",
+    ememyAttackBack: 15
+  },
+  "Luke Skywalker": {
+    name: "Luke Skywalker",
+    health: 100,
+    attack: 14,
+    imageUrl: "assets/images/luke-skywalker.jpg",
+    enemyAttackBack: 5
+  },
+  "Darth Sidious": {
+    name: "Darth Sidious",
+    health: 150,
+    attack: 8,
+    imageUrl: "assets/images/darth-sidious.png",
+    enemyAttackBack: 20
+  },
+  "Darth Maul": {
+    name: "Darth Maul",
+    health: 180,
+    attack: 7,
+    imageUrl: "assets/images/darth-maul.jpg",
+    enemyAttackBack: 25
+  }
+};
+
+
+
+
+
+})
